71a5353382c2565636458d6935f97bc078a6d770
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-08 17:37:23 +0200

    bm: TaskController

--- src/main/java/hu/progmatic/thymeleafpractice/controller/TaskController.java
+++ src/main/java/hu/progmatic/thymeleafpractice/controller/TaskController.java
@@ -1,8 +1,24 @@
 package hu.progmatic.thymeleafpractice.controller;
 
+import hu.progmatic.thymeleafpractice.model.BlogEntry;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class TaskController {
+
+    @GetMapping("/task1")
+    public String task1(Model model) {
+        BlogEntry blogEntry = new BlogEntry(
+                "Title1", "Content1", "Java",
+                5, true, LocalDate.now()
+        );
+        model.addAttribute("entry", blogEntry);
+
+        return "task1";
+    }
 
 }
